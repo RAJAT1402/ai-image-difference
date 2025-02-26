b75e24ba145d127da5485385a9861b822f5e2241
rajat <rajat@hood.live>
2025-02-26 10:15:03 +0530

    new test

--- src/app.service.ts
+++ src/app.service.ts
@@ -58,13 +58,13 @@ export class AppService {
           content: [
             // { type: "text", text: "Analyize the photo" },
             { type: "text", text: prompt },
-            // { type: "image_url", image_url: { url: "https://zorro-bucket.s3.amazonaws.com/New_Project/ai-image/1740208185920-IMG_0123.jpg" } }, // First image
-            // { type: "image_url", image_url: { url: "https://zorro-bucket.s3.ap-south-1.amazonaws.com/New_Project/ai-image/1740208187631-IMG_0124.jpg" } }, // First image
-            // { type: "image_url", image_url: { url: "https://zorro-bucket.s3.ap-south-1.amazonaws.com/New_Project/ai-image/1740208188419-IMG_0127.jpg" } }, // First image
+            { type: "image_url", image_url: { url: "https://zorro-bucket.s3.amazonaws.com/New_Project/ai-image/1740208185920-IMG_0123.jpg" } }, // First image
+            { type: "image_url", image_url: { url: "https://zorro-bucket.s3.ap-south-1.amazonaws.com/New_Project/ai-image/1740208187631-IMG_0124.jpg" } }, // First image
+            { type: "image_url", image_url: { url: "https://zorro-bucket.s3.ap-south-1.amazonaws.com/New_Project/ai-image/1740208188419-IMG_0127.jpg" } }, // First image
           
-            { type: "image_url", image_url: { url: image1 } }, // First image
-            { type: "image_url", image_url: { url: image2 } }, // Second image
-            { type: "image_url", image_url: { url: image3 } }  // Third image
+            // { type: "image_url", image_url: { url: image1 } }, // First image
+            // { type: "image_url", image_url: { url: image2 } }, // Second image
+            // { type: "image_url", image_url: { url: image3 } }  // Third image
           ],
         },
       ],
